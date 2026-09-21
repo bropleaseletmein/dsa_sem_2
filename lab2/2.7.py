0497779e1db11fd4046ea20ed6af90153423c78f
bropleaseletmein <andrew009823464@outlook.com>
2026-09-21 14:49:11 +0300

    lab 2.7

---
+++ lab2/2.7.py
@@ -0,0 +1,29 @@
+n = int(input())
+key = [0] * n
+left = [0] * n
+right = [0] * n
+
+for i in range(n):
+   k, l, r = map(int, input().split())
+   key[i] = k
+   left[i] = l
+   right[i] = r
+
+inf = float('inf')
+ok = True
+stack = [(0, -inf, inf)] if n > 0 else []
+
+while stack:
+   v, lo, hi = stack.pop()
+   if not lo <= key[v] < hi:
+       ok = False
+       break
+   if left[v] != -1:
+       stack.append((left[v], lo, key[v]))
+   if right[v] != -1:
+       stack.append((right[v], key[v], hi))
+
+if ok:
+   print("CORRECT")
+else:
+   print("INCORRECT")
